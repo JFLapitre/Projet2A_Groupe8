c09b43d2aea4421e58717e8e12d8d80159fe814c
sachabjn <sacha.beaujean@eleve.ensai.fr>
2025-10-03 15:14:49 +0000

    Test d'AdminMenuService

--- tests/Service/test_AdminMenuService.py
+++ tests/Service/test_AdminMenuService.py
@@ -1,10 +1,11 @@
-
 import pytest
 from src.services.admin_menu_service import AdminMenuService
+
 
 @pytest.fixture
 def admin_menu_service():
     return AdminMenuService()
+
 
 @pytest.mark.parametrize(
     "name, desc, price, stock, availability, should_raise_error",
@@ -12,15 +13,15 @@ def admin_menu_service():
         # Valid cases
         ("Valid Item", "Description", 10.99, 100, True, False),
         ("Valid Item", "Description", 10.99, 0, False, False),
-
         # Invalid cases
-        ("Invalid Item", "Description", 10.99, 0, True, True),     # Stock is null but product is available
-        ("", "Description", 10.99, 100, True, True),               # No name
+        ("Invalid Item", "Description", 10.99, 0, True, True),  # Stock is null but product is available
+        ("", "Description", 10.99, 100, True, True),  # No name
         ("Invalid Item", "Description", -10.99, 100, True, True),  # Negative price
     ],
 )
 def test_create_item_with_various_inputs(
-    admin_menu_service, name, desc, price, stock, availability, should_raise_error):
+    admin_menu_service, name, desc, price, stock, availability, should_raise_error
+):
     if should_raise_error:
         with pytest.raises(ValueError):
             admin_menu_service.create_item(name, desc, price, stock, availability)
