2fa5e09b0c0a39cbff9dfcf715a49e5d5f3ef716
Joanes Demory <joanesdemory@gmail.com>
2025-10-25 10:46:09 +0200

    Des tests

---
+++ test_find_all.py
@@ -0,0 +1,34 @@
+from src.DAO.userDAO import UserDAO  
+from src.DAO.DBConnector import DBConnector
+import logging
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configure le logging pour voir les erreurs
+logging.basicConfig(level=logging.INFO)
+
+# Initialise le connecteur à la base de données
+db_connector = DBConnector()
+
+# Crée une instance de UserDAO
+userDAO = UserDAO(db_connector)
+
+
+try:
+    
+    user_dao = UserDAO(db_connector) 
+    users = user_dao.find_all()
+
+    if users is not None:
+        print("✅ Utilisateurs trouvés :")
+        for user in users:
+            print(f"  - ID: {user.id_user}")
+            print(f"  - Username: {user.username}")
+
+    else:
+        print("❌ Aucun utilisateur trouvé avec cet ID.")
+
+except Exception as e:
+    print(f"⚠️ Erreur lors de la récupération de l'utilisateur : {e}")
