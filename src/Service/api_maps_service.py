67b3e6f64e90f974722e2804868cb72afe468531
JFLapitre <jflapitre@gmail.com>
2025-11-04 15:53:09 +0000

    Modif API Maps

--- src/Service/api_maps_service.py
+++ src/Service/api_maps_service.py
@@ -22,59 +22,12 @@ order_dao = OrderDAO(db_connector=db, user_dao=user_dao, address_dao=address_dao
 delivery_dao = DeliveryDAO(db_connector=db, user_dao=user_dao, order_dao=order_dao)
 
 
-address1 = address_dao.find_address_by_id(1)
-print(address1)
-# Exemple : on récupère une commande
-order = order_dao.find_order_by_id(1)
-
-
-origin: str = f"{order.address.street_number},{order.address.street_name}, {order.address.city}"
-
-print(origin)
-
-
 API_KEY = "AIzaSyBgOvV_du58_DMUTf7O8ACDt3SQ_USfeXE"
 
-origin = "Rue des cerisiers,Longjumeau,France"
-destination = "Bruz,France"
-waypoints: list[str] = ["Marseille,France"]
 
-
-url: str = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&waypoints={waypoints}&key={API_KEY}"
-
-response = requests.get(url)
-data = response.json()
-
-
-# Exemple : afficher la distance et la durée
-if data["status"] == "OK":
-    route = data["routes"][0]
-    legs = route.get("legs", [])
-    maps_url = (
-        "https://www.google.com/maps/dir/?api=1"
-        f"&origin={urllib.parse.quote(origin)}"
-        f"&destination={urllib.parse.quote(destination)}"
-        f"&waypoints={'|'.join([urllib.parse.quote(w) for w in waypoints])}"
-    )
-
-    total_distance_m = sum(leg["distance"]["value"] for leg in legs)
-    total_duration_s = sum(leg["duration"]["value"] for leg in legs)
-    total_distance_km = total_distance_m / 1000  # km
-    hours, remainder = divmod(total_duration_s, 3600)
-    minutes, seconds = divmod(remainder, 60)
-
-    print(f"Distance totale : {total_distance_km:.2f} km")
-    print(f"Durée totale : {hours}h {minutes}min {seconds}s")
-    print("Lien Google Maps :", maps_url)
-
-
-else:
-    print("Erreur :", data["status"])
-
-
-class ApiMApsService:
+class ApiMapsService:
     def __init__(self) -> None:
-        self.user_dao = UserDAO()
+        self.delivery_dao = DeliveryDAO()
 
     def Driveritinerary(destination, waypoints=[]):
         API_KEY = "AIzaSyBgOvV_du58_DMUTf7O8ACDt3SQ_USfeXE"
@@ -130,5 +83,4 @@ class ApiMApsService:
             else:
                 print("Erreur :", data["status"])
 
-
-Driveritinerary("18 Rue Charles Coudé, Bruz, France", waypoints=["17 Rue Jules lallemand, Rennes, France"])
+    Driveritinerary("18 Rue Charles Coudé, Bruz, France", waypoints=["17 Rue Jules lallemand, Rennes, France"])
