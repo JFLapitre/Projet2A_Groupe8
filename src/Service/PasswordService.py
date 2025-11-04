0577363119302d760dc6812b2abd60d6ffaee001
justincg27 <justin.cochet-garsiot@eleve.ensai.fr>
2025-11-04 14:05:01 +0000

    password_service

--- src/Service/PasswordService.py
+++ src/Service/PasswordService.py
@@ -7,22 +7,23 @@ from src.Model.abstract_user import AbstractUser
 
 
 def hash_password(password: str, salt: Optional[str] = None) -> str:
-    ## TODO
-
-    return "1234"
+    return hashlib.sha256((password + salt).encode()).hexdigest()
 
 
 def create_salt() -> str:
-    return secrets.token_hex(128)
+    return secrets.token_hex(32)
 
 
-def check_password_strength(password: str):
+def check_password_strength(password: str):  # à modifier à l'avenir
     if len(password) < 8:
         raise Exception("Password length must be at least 8 characters")
 
 
 def validate_username_password(username: str, password: str, user_DAO: UserDAO) -> AbstractUser:
-    user_with_username: Optional[AbstractUser] = user_DAO.get_by_username(username=username)
-    ## TODO
+    user = user_DAO.find_user_by_username(username=username)
+    if not user:
+        raise ValueError("User not found.")
+    if hash_password(password, user.salt) != user.hash_password:
+        raise ValueError("Incorrect password.")
 
-    return user_with_username
+    return user
