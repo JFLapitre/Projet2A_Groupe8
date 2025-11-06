6e5b1fecf3bdd87f3a9fbbb0e4e86aa3ac5fc8fd
JFLapitre <jflapitre@gmail.com>
2025-11-06 09:55:08 +0000

    Driver service (fonctionel)

--- src/Service/driver_service.py
+++ src/Service/driver_service.py
@@ -144,7 +144,3 @@ class DriverService:
         return [d for d in all_deliveries if d.status == "pending"]
 
 
-db_connector = DBConnector()
-service = DriverService(db_connector)
-service.create_and_assign_delivery([3],3)
-service.get_itinerary(3)
