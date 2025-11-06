f039b50ada4ca23b43ff141eee9f3d655ae0b15e
sachabjn <sacha.beaujean@eleve.ensai.fr>
2025-11-06 10:20:55 +0000

    test_addressDAO

---
+++ tests/DAO/test_addressDAO.py
@@ -0,0 +1,58 @@
+import pytest
+from typing import Optional, Union, Literal, Dict, Any, List
+from datetime import date
+from src.DAO.addressDAO import AddressDAO
+from src.Model.address import Address
+
+class MockDBConnector:
+    def __init__(self):
+        self.address = [{"id_address": 1, "city": 'Lyon', "postal code": 69000, "street name": 'Rue de la République',
+        "street number": "1" },{"id_address": 2, "city": 'Auxerre', "postal code": 89000, "street name": 'Rue des Boussicats',
+        "street number":"3 bis"}]
+        self.next_adress_id = 3
+
+    def sql_query(self,
+                  query: str,
+                  data: Optional[Union[tuple, list, dict]] = None, 
+                  return_type: Optional[Union[Literal["one"], Literal["all"], None]] = "one"
+    ) -> Union[Dict[str, Any], List[Dict[str, Any]], bool, None]:
+
+            q =" ".join(query.lower().split())
+
+            # find address
+            if "from fd.address" in q and "where fd.adress" in q and return_type == "one":
+                address_id = None
+                if isinstance(data, dict):
+                    address_id = data.get("id address")
+                elif isinstance(data, (list, tuple)):
+                    if data:
+                        address_id = data[0]
+                if address_id:
+                    for address in self.address:
+                        if address["id address"] == address_id:
+                            return address.copy() 
+                return None
+
+            # find all
+            if "select * from fd.address" in q and return_type == "all":
+                return list(self.address)
+
+            # add one address
+            if "insert into fd.address" in q:
+                if not isinstance(data, dict):
+                    return False
+
+                new_id = self.next_address_id
+                self.next_address_id += 1
+
+                created_address = {
+                    "id_address": new_id,
+                    "city": data["city"],
+                    "postal code": data[""],
+                    "street name": 'Rue de la République',
+                    "street number": "1"
+                }
+
+                self.items.append(created_item) 
+                return created_item
+
