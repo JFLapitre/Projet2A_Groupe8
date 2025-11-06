92e2545fe85d948124f322268f7691f32cbedfdf
justincg27 <justin.cochet-garsiot@eleve.ensai.fr>
2025-11-06 12:23:30 +0000

    cli pb address

--- src/Model/address.py
+++ src/Model/address.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel
 
 
 class Address(BaseModel):
-    id_address : Optional[int] = None
+    id_address: Optional[int] = None
     city: str
     postal_code: int
     street_name: str
-    street_number: Optional[str] = None
+    street_number: Optional[str | int] = None
